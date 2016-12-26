be82a554467fbc98400b7ba67462793fc601a617
Thales Macedo Garitezi <thalesmg@gmail.com>
2016-12-26 07:35:38 -0200

    Initial commit

---
+++ test/mars_exploration/core_test.clj
@@ -0,0 +1,7 @@
+(ns mars-exploration.core-test
+  (:require [clojure.test :refer :all]
+            [mars-exploration.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
